d297a6d3cc3bd3004c34a79de5d0551424cbe13e
Jaime Flores <jaimelopesflores@gmail.com>
2018-11-30 21:12:16 -0200

    preparing to npm publish

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,11 @@ module.exports = (env) => {
     mode: 'production',
     entry: path.resolve(path.resolve(__dirname, 'src'), 'index.js'),
     output: {
-      path: path.resolve(__dirname, 'build'),
+      path: path.resolve(__dirname, 'dist'),
       filename: 'index.js',
       library: ['react-webrtc-adapter'],
       libraryTarget: 'umd',
-      publicPath: 'build'
+      publicPath: 'dist'
     },
     module: {
       rules: [
