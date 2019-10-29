5211dfb3a89fc9be05b1b6688cfca45db9d8dd22
Lucas Frezarini <lucas.frezarini@teravoz.com.br>
2019-10-29 10:31:12 -0300

    Audit fix and allowing to pass the headers options to the client

--- src/index.js
+++ src/index.js
@@ -19,6 +19,8 @@ export default (WrappedComponent, opts) => {
       this.hangUp = handle('hangUp');
       this.sendDTMF = handle('sendDTMF');
       this.setDevices = handle('setDevices');
+      this.destroy = handle('destroy');
+      this.refresh = handle('refresh');
     }
   }
 
@@ -28,6 +30,7 @@ export default (WrappedComponent, opts) => {
       super(props);
       this.teravozAudioLocalStream = createRef();
       this.teravozAudioRemoteStream = createRef();
+
       this.state = {
         events: new EventEmitter(),
         actions: null,
@@ -63,6 +66,7 @@ export default (WrappedComponent, opts) => {
 
       Teravoz.start({
         origin: opts.origin || null,
+        headers: opts.headers || null,
         gatewayCallbacks: {
           closed: eventHandler('gateway-closed'),
           error: eventHandler('gateway-error'),
